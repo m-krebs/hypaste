02dcd02ccead915e89f0ca428d6f680532ce6242
Michael Krebs <michikrbs@gmail.com>
2025-04-19 00:19:16 +0200

    refactor(db): update schema

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,15 @@
-import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
+import { sqliteTable, text, integer, blob, index } from 'drizzle-orm/sqlite-core';
 
-export const user = sqliteTable('user', {
-	id: integer('id').primaryKey(),
-	age: integer('age')
-});
+export const author = sqliteTable('author', {
+	id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
+})
+
+export const paste = sqliteTable('paste', {
+	id: text('id').primaryKey(),
+	created: integer('created', { mode: 'timestamp_ms' }),
+	deleteAt: integer('deleteAt', { mode: 'timestamp_ms' }),
+	authorId: integer('author_id').references(() => author.id),
+	content: blob('content')
+}, (table) => [
+	index('delete_at_idx').on(table.deleteAt),
+]);
