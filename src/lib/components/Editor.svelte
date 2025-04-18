b24573425524307c2c37a0bce86d781160a877ed
Michael Krebs <michikrbs@gmail.com>
2025-04-19 00:18:34 +0200

    refactor: editor

---
+++ src/lib/components/Editor.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { EditorView, basicSetup } from 'codemirror';
+	import { javascript } from '@codemirror/lang-javascript';
+	import { onMount } from 'svelte';
+
+	let editorContainer: HTMLElement;
+
+	onMount(() => {
+		let editor = new EditorView({
+			extensions: [basicSetup, javascript()],
+			parent: editorContainer
+		});
+	});
+</script>
+
+<div bind:this={editorContainer}></div>
