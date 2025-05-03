8382fedb1a3f94f7af03b094a1e4a67949147a29
Michael Krebs <michikrbs@gmail.com>
2025-05-03 02:49:51 +0200

    fix: schema not added to db const

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,4 +3,5 @@ import { createClient } from '@libsql/client';
 import { env } from '$env/dynamic/private';
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 const client = createClient({ url: env.DATABASE_URL });
-export const db = drizzle(client);
+import * as schema from './schema';
+export const db = drizzle(client, { schema });
